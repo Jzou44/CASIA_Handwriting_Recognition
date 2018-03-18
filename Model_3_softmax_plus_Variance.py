a007fc64b3bc17d1a0fc7e1289ac9582143fc708
jzou44-surfacebook2 <jzou44@uwo.ca>
2018-03-18 01:44:04 -0400

    add Model_3_softmax_plus_Variance

--- Model_3_softmax_plus_Variance.py
+++ Model_3_softmax_plus_Variance.py
@@ -25,11 +25,11 @@ classification_layer = NN_Model.full_connected_classifier(feature_layer)
 
 optimizer = tf.train.AdamOptimizer(learning_rate=0.001, epsilon=1)
 
-loss_euclidean = calculate_variance(classification_layer)
+loss_variance = calculate_variance(classification_layer)
 #
 loss_softmax = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=classification_layer, labels=Y))
-loss_softmax_plus_euclidean = tf.add(loss_softmax, tf.multiply(1.0, loss_euclidean))
-train_op = optimizer.minimize(loss_softmax_plus_euclidean)
+loss_softmax_plus_variance = tf.add(loss_softmax, tf.multiply(0.1, loss_variance))
+train_op = optimizer.minimize(loss_softmax_plus_variance)
 # Initializing the variables
 init = tf.global_variables_initializer()
 with tf.Session() as sess:
@@ -46,9 +46,9 @@ with tf.Session() as sess:
         batch_round = 1
         while True:
             batch_x, batch_y = dm.next_train_batch_fix_character_amount(character_amount, each_character_sample_amount)
-            _, cost, cost_s, cost_e = sess.run([train_op, loss_softmax_plus_euclidean, loss_softmax, loss_euclidean],
+            _, cost, cost_s, cost_v = sess.run([train_op, loss_softmax_plus_variance, loss_softmax, loss_variance],
                                                feed_dict={X: batch_x, Y: batch_y})
-            log.info("batch_round: {0},cost={1},softmax={2},euclidean={3}".format(batch_round, cost, cost_s, cost_e))
+            log.info("batch_round: {0},cost={1},softmax={2},euclidean={3}".format(batch_round, cost, cost_s, cost_v))
             if np.mod(batch_round, 100) == 0:
                 save_path = saver.save(sess, weight_path)
                 log.info('save weight')
